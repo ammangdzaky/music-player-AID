1738fe5674840e96c2332be217f6c653f86658f5
indirafix03 <indiraramayani14@gmail.com>
2025-06-12 17:10:08 +0800

    refactor (Profile scene 75%)

--- src/main/java/aid/Main.java
+++ src/main/java/aid/Main.java
@@ -1,7 +1,16 @@
 package aid;
 
-public class Main {
+import aid.controllers.ProfileController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        new ProfileController().show(primaryStage);
+    }
+
     public static void main(String[] args) {
-        
+        launch(args);
     }
 }
--- src/main/java/aid/models/Song.java
+++ src/main/java/aid/models/Song.java
@@ -1,4 +1,14 @@
 package aid.models;
 
 public class Song {
+    public int id;
+    public String title;
+    public String artist;
+    public String file;
+    public String cover;
+
+    @Override
+    public String toString() {
+        return title + " by " + artist;
+    }
 }
